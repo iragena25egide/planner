c54308deecc6af0d2f10a53c8f7696e42b47c8d3
unknown <iragenaegide205@gmail.com>
2026-03-23 20:26:15 +0200

    new onboarding

--- planner/src/screens/onBoardingCoursel.tsx
+++ planner/src/screens/onBoardingCoursel.tsx
@@ -13,7 +13,7 @@ import { LinearGradient } from 'expo-linear-gradient';
 import { useNavigation } from '@react-navigation/native';
 import type { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../navigation/AppNavigator';
-import { slides } from '../data/onboardingData';
+import { slides } from '../data/onboarding';
 
 const { width, height } = Dimensions.get('window');
 
