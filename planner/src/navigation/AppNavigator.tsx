0b663c51f210dc8ca66741cd022ebad6258f9c3a
unknown <iragenaegide205@gmail.com>
2026-03-24 12:51:23 +0200

    app navigator

--- planner/src/navigation/AppNavigator.tsx
+++ planner/src/navigation/AppNavigator.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import OnboardingScreen from '../screens/OnboardingScreen';
+import onBoardingCoursel from '../screens/onBoardingCoursel';
 import AppTabs from './AppTabs';
 
 export type RootStackParamList = {
@@ -16,7 +17,7 @@ export default function AppNavigator() {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Onboarding" component={OnboardingScreen} />
+        <Stack.Screen name="Onboarding" component={onBoardingCoursel} />
         <Stack.Screen name="Home" component={AppTabs} />
       </Stack.Navigator>
     </NavigationContainer>
