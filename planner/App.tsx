436457144e6e7f22977c9cc0ee7e974ed2acbe45
unknown <iragenaegide205@gmail.com>
2026-03-25 16:39:56 +0200

    the main app.tsx act as the main file to be rendered for this mobille application

--- planner/App.tsx
+++ planner/App.tsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import AppNavigator from './src/navigation/AppNavigator';
+import { TaskProvider } from './src/context/TaskContext';
+import { registerForPushNotificationsAsync } from './src/services/notifications';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function App() {
+  useEffect(() => {
+    registerForPushNotificationsAsync();
+  }, []);
+
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <AppNavigator />
+      <SafeAreaProvider>
+        <TaskProvider>
+          <StatusBar style="auto" />
+          <AppNavigator />
+        </TaskProvider>
+      </SafeAreaProvider>
     </GestureHandlerRootView>
   );
 }
